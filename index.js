88ae7494c807ab01a5eb3c4a3a2f22345bc61e76
Lars Cornelissen <l.cornelissen5@student.han.nl>
2023-05-31 17:10:28 +0200

    fixed error handling

--- index.js
+++ index.js
@@ -6,33 +6,47 @@ const prefix = "https://www.youtube.com/watch?v=";
 const allowed_types = ['mp4', 'webm', 'ogg'];
 
 app.get('/:id/:type', (req, res) => {
-    const id = req.params.id;
-    const url = prefix + id
-    if (!allowed_types.includes(req.params.type)) {
-        // Reset headers
+    try {
+        const id = req.params.id;
+        const url = prefix + id
+        if (!allowed_types.includes(req.params.type)) {
+            // Reset headers
+            res.set({
+                'Content-Type': 'text/html',
+                'Content-Disposition': ''
+            });
+            
+            return res.send('Invalid format, allowed: ' + allowed_types.join(', '));
+        }
+        console.log(`Downloading ${id}.${req.params.type}...`);
+
+        // get video name
+        ytdl.getInfo(url).then(info => {
+            const title = info.videoDetails.title;
+            res.header('Content-Disposition', `attachment; filename=${title}.${req.params.type}`);
+            ytdl(url, {
+                format: req.params.type,
+                filter: 'audioandvideo',
+                quality: 'highestvideo',
+                format: 'hd1080'
+            }).pipe(res);
+        })
+        .catch(err => {
+            console.log(err);
+            res.set({
+                'Content-Type': 'text/html',
+                'Content-Disposition': ''
+            });
+            res.send('Error while getting youtube data! (check video id?)');
+        });
+    } catch (err) {
+        console.log(err);
         res.set({
             'Content-Type': 'text/html',
             'Content-Disposition': ''
         });
-        
-        return res.send('Invalid format, allowed: ' + allowed_types.join(', '));
+        res.send('Error: ' + err);
     }
-    console.log(`Downloading ${id}.${req.params.type}...`);
-
-    // get video name
-    ytdl.getInfo(url).then(info => {
-        const title = info.videoDetails.title;
-        res.header('Content-Disposition', `attachment; filename=${title}.${req.params.type}`);
-        ytdl(url, {
-            format: req.params.type,
-            filter: 'audioandvideo',
-            quality: 'highestvideo',
-            format: 'hd1080'
-        }).pipe(res);
-    });
-
-
-
 });
 
 app.listen(port, () => {
